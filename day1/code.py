0af833834b1d8076cfc2b7f75c590c6de37cbbd9
Hélène Roggeman <helene.roggeman@gmail.com>
2024-12-01 10:25:09 +0100

    day1 add code

--- day1/code.py
+++ day1/code.py
@@ -5,5 +5,18 @@ file_results = 'example'
 f = open(file_results,'r')
 lines = list(f)
 
-sol = 0
-print("res part 1: ", sol)
+left_values, right_values = zip(*[[int(e) for e in line.split()] for line in lines])
+
+# Part 1
+res = 0
+for a,b in zip(sorted(left_values), sorted(right_values)):
+    res += abs(a-b)
+
+print("res part 1: ", res)
+
+# Part 2
+res2 = 0
+for e in left_values:
+    res2 += e * right_values.count(e)
+    
+print("res part 2: ", res2)
