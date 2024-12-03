aa20956d7739dfea179d5bc8552f9e6df4e36e0e
Hélène Roggeman <helene.roggeman@gmail.com>
2024-12-03 10:14:01 +0100

    day3 add code

---
+++ day3/code.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import re
+
+file_results = 'input'
+
+f = open(file_results,'r')
+line = f.read()
+
+#Part 1
+def compute_mul(line):
+    sequences = re.findall("mul\([0-9]+,[0-9]+\)", line)
+    res = 0
+    for seq in sequences:
+        a, b = [int(e) if int(e) < 1000 else 0 for e in re.findall("[0-9]+", seq)]
+        res += a*b
+    return res
+
+print("res part 1: ", compute_mul(line))    
+
+
+#Part 2
+split_dont = re.split("don't()", line)
+res2 = compute_mul(split_dont[0])
+
+for split_dont_element in split_dont[1:]:
+    if not split_dont_element:
+        continue
+    split_do = re.split("do()", split_dont_element)
+    for seq in split_do[1:]:
+        res2 += compute_mul(seq)
+
+print("res part 2: ", res2)
