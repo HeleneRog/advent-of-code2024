87d2e287e36093514f716078a7bd05486e7e7e92
Hélène Roggeman <helene.roggeman@easymile.com>
2024-12-09 13:53:02 +0100

    day9 rework part2 (OK)

--- day9/code.py
+++ day9/code.py
@@ -45,50 +45,33 @@ print("Part1:", res_part1)
         
 res_part2 = 0
 
-block_sizes = [e for index, e in enumerate(line) if (index % 2) == 0]
-max_file = int((N-1)/2)
-                 
-def find_first_valid_blocks(size, block_moved_indexes):
-    res = []
-    while size > 0:
-        found_sol = False
-        for index, e in enumerate(block_sizes[::-1]):
-            if (index in block_moved_indexes):
-                continue
-            if (e <= size):
-                res.append([e, max_file-index])
-                block_moved_indexes.add(index)
-                size -= e
-                found_sol = True
-                break
-        if (not found_sol):
-            break
-    return res, size
-
-block_moved_indexes = set()
-cur_position = 0
-cur_file = 0
-
-for index, number in enumerate(line):
-    if (index % 2) == 0:
-        block_reverse_index = int((N-index) / 2)
-        if block_reverse_index in block_moved_indexes:
-            cur_file += 1
-            cur_position += block_sizes[int(index/2)]
-            continue
-        for j in range(number):
-            res_part2 += cur_position * cur_file
-            cur_position += 1
-        cur_file += 1
-    else:
-        sizes, remaining = find_first_valid_blocks(number, block_moved_indexes)
-        for size, file in sizes:
-            for j in range(size):
-                res_part2 += cur_position * file
-                cur_position += 1
-        cur_position += remaining
+class Block:
+    def __init__(self, size):
+        self.size = size
+        self.position = None
+        
+    def set_position_and_compute_score(self, position, file):
+        self.position = position
+        return compute_score(position, file, self.size)
     
+blocks_map = dict([(int(index/2), Block(e)) for index, e in enumerate(line) if (index % 2) == 0])
+position = 0
+
+for index, size in enumerate(line):
+    if (index % 2) == 0:
+        file = index / 2
+        if not blocks_map[file].position:
+            res_part2 += blocks_map[file].set_position_and_compute_score(position, file)
+        position += blocks_map[file].size
+    else:
+        while size > 0:
+            try:
+                file, block = next((f, b) for (f, b) in list(blocks_map.items())[::-1] if (not b.position and b.size <= size))
+                res_part2 += blocks_map[file].set_position_and_compute_score(position, file)
+                position += block.size
+                size -= block.size
+            except StopIteration:
+                position += size
+                break
         
 print("Part2:", res_part2)
-
-#10923582871568 too high
