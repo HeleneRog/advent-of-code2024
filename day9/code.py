5af272478b48648a32348a9958535dbb6d8642d6
Hélène Roggeman <helene.roggeman@easymile.com>
2024-12-09 13:52:43 +0100

    day9 rework part1

--- day9/code.py
+++ day9/code.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python
-import numpy as np
 
-file = 'example'
+file = 'input'
            
 f = open(file,'r')
 line = [int(e) for e in list(f)[0].rstrip()]
-
 N = len(line)
 
+def compute_score(position, file, size):
+    return file*(size*position+size*(size-1)/2)
+
 res_part1 = 0
-cur_file = 0
 
 reverse_index = N-1
-reverse_number = line[reverse_index]
-max_file = int((N-1)/2)
-cur_position = 0
+reverse_size = line[reverse_index]
 
-for index, number in enumerate(line):
+file = 0
+reverse_file = int((N-1)/2)
+
+position = 0
+
+for index, size in enumerate(line):
     if (index % 2) == 0:
-        if (cur_file == max_file):
-            number = reverse_number
-        for j in range(number):
-            res_part1 += cur_position * cur_file
-            cur_position += 1
-        cur_file += 1
+        if (file == reverse_file):
+            size = reverse_size
+        res_part1 += compute_score(position, file, size)
+        position += size
+        file += 1
     else:
         count = 0
-        while(count < number):
-            res_part1 += cur_position * max_file
-            cur_position += 1
+        while(count < size):
+            res_part1 += position * reverse_file
+            position += 1
             count += 1
-            reverse_number -= 1
-            if reverse_number == 0:
-                max_file -= 1
+            reverse_size -= 1
+            if reverse_size == 0:
+                reverse_file -= 1
                 reverse_index -= 2
-                reverse_number = line[reverse_index]
-    if (cur_file > max_file):
+                reverse_size = line[reverse_index]
+    if (file > reverse_file):
         break
 
 print("Part1:", res_part1)
